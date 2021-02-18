38d18cc34740cbb68138b9e84f812b41ee0fda06
Lorenzo Kenon <70530138+s10990323@users.noreply.github.com>
2021-02-18 12:32:40 -0500

    7B v0.1

--- dice_role.py
+++ dice_role.py
@@ -1,4 +1,14 @@
-# Dice roll Library, Lorenzo Kenon, 02/18/2021, 12:22PM, v0.0
+# Dice roll Library, Lorenzo Kenon, 02/18/2021, 12:32PM, v0.1
 
+
+# D4 Simulator
+def roll_d4(num_roll): # num_roll is an ARGUEMENT.
+    import random
+    
+    rolls = 0 
+    while rolls <= num_roll:
+        result = random.randint (1,4)
+        print(f"You rolled a {result} on the d4!\n")
+        rolls +=  1
 
 
