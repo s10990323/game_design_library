f3418938ec928656e34e7842a749edd2a8434645
Lorenzo Kenon <70530138+s10990323@users.noreply.github.com>
2021-02-18 12:49:12 -0500

    7B v0.2

--- dice_role.py
+++ dice_role.py
@@ -1,14 +1,20 @@
-# Dice roll Library, Lorenzo Kenon, 02/18/2021, 12:32PM, v0.1
+# Dice roll Library, Lorenzo Kenon, 02/18/2021, 12:48PM, v0.2
 
 
 # D4 Simulator
 def roll_d4(num_roll): # num_roll is an ARGUEMENT.
     import random
-    
+    import time
+
     rolls = 0 
-    while rolls <= num_roll:
+    sum = 0
+    while rolls < num_roll:
         result = random.randint (1,4)
         print(f"You rolled a {result} on the d4!\n")
         rolls +=  1
+        time.sleep(1)
+        sum += result
+    print(f"The total roll was {sum}.\n")
 
+roll_d4(8)
 
