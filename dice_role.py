ec263bdc822311cb58d40f7093e4b0ab4ee19515
Lorenzo Kenon <70530138+s10990323@users.noreply.github.com>
2021-02-18 12:54:10 -0500

    7B v0.3
    Added d rolls 6,8,12,20,100

--- dice_role.py
+++ dice_role.py
@@ -1,4 +1,4 @@
-# Dice roll Library, Lorenzo Kenon, 02/18/2021, 12:48PM, v0.2
+# Dice roll Library, Lorenzo Kenon, 02/18/2021, 12:53PM, v0.3
 
 
 # D4 Simulator
@@ -16,5 +16,45 @@ def roll_d4(num_roll): # num_roll is an ARGUEMENT.
         sum += result
     print(f"The total roll was {sum}.\n")
 
-roll_d4(8)
+#roll_d4(8)
+
+    while rolls < num_roll:
+        result = random.randint (1,6)
+        print(f"You rolled a {result} on the d4!\n")
+        rolls +=  1
+        time.sleep(1)
+        sum += result
+    print(f"The total roll was {sum}.\n")
+
+    while rolls < num_roll:
+        result = random.randint (1,8)
+        print(f"You rolled a {result} on the d4!\n")
+        rolls +=  1
+        time.sleep(1)
+        sum += result
+    print(f"The total roll was {sum}.\n")
+
+    while rolls < num_roll:
+        result = random.randint (1,12)
+        print(f"You rolled a {result} on the d4!\n")
+        rolls +=  1
+        time.sleep(1)
+        sum += result
+    print(f"The total roll was {sum}.\n")
+
+    while rolls < num_roll:
+        result = random.randint (1,20)
+        print(f"You rolled a {result} on the d4!\n")
+        rolls +=  1
+        time.sleep(1)
+        sum += result
+    print(f"The total roll was {sum}.\n")
+
+    while rolls < num_roll:
+        result = random.randint (1,100)
+        print(f"You rolled a {result} on the d4!\n")
+        rolls +=  1
+        time.sleep(1)
+        sum += result
+    print(f"The total roll was {sum}.\n")
 
